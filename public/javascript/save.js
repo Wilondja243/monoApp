cd99a1b0ff85103a4ea485beb6bbbf160bf32585
Luckson <164260676+Wilondja243@users.noreply.github.com>
2024-06-13 17:09:09 +0200

    Add files via upload
    projet d'un site de streaming video en cours d'execution

---
+++ public/javascript/save.js
@@ -0,0 +1,13 @@
+// function writeText(){
+    //     if(index < textH1.length){
+    //         h1.textContent += textH1.charAt(index);
+    //     }
+    //     if(index < textPara.length){
+    //         paraf.textContent += textPara.charAt(index);
+    //     }
+    //     if(index <= textH1.length || index <= textH1.length){
+    //        index++;
+    //        setTimeout(writeText,100);
+    //     }
+    //  }
+    //  writeText();
